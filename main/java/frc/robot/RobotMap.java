02dcd58ac5cb96e746187a7dc57b8dbea39dbb16
NeilMcNeil <119974057+NeilMcNeil@users.noreply.github.com>
2024-03-08 19:56:44 -0800

    Update RobotMap.java

--- main/java/frc/robot/RobotMap.java
+++ main/java/frc/robot/RobotMap.java
@@ -12,6 +12,36 @@ public class RobotMap {
     public static final int lifterArmMotor = 7;
 
     //shooter IDs
-    public static final int leftShooterID = 8;
-    public static final int rightShooterID = 9;
+    public static final int leftShooterID = 1;
+    public static final int rightShooterID = 2;
+
+    //intake motors
+    public static final int bottomIntakeMotorId = 9;
+    public static final int topIntakeMotorId = 8;
+
+    // lifter motor, pneumatics stuff
+    public static final int liftUpID = 12;
+    public static final int liftDownID = 13;
+
+    // operator joystick mapping
+    public static final int x = 1;
+    public static final int a = 2;
+    public static final int b = 3;
+    public static final int y = 4;
+
+    public static final int lt = 7;
+    public static final int rt = 8;
+    public static final int back = 9;
+
+    // normal joystick mapping
+    public static final int trigger = 1;
+    public static final int speedShiftButton = 2;
+
+
+    // this is a place for constants
+    public static final double fastSpeed = 1;
+    public static final double slowSpeed = 0.25;
+    public static final double intakeSpeed = 0.25;
+    public static final double shootSpeed = 0.75;
+    public static final double shooterIntakeSpeed = -0.25;
 }
