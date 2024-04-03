5bab69fed928d16f0c72737307c609d98a3b9aa9
NeilMcNeil <119974057+NeilMcNeil@users.noreply.github.com>
2024-04-03 12:33:20 -0700

    Update Robot.java
    after humber, encoders and new autos kinda sort of working

--- main/java/frc/robot/Robot.java
+++ main/java/frc/robot/Robot.java
@@ -7,19 +7,27 @@ package frc.robot;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.Compressor;
+//import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.Compressor;
+//import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.PneumaticsControlModule;
+//import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -48,9 +56,12 @@ public class Robot extends TimedRobot {
 
   //PneumaticsControlModule pcm = new PneumaticsControlModule(0);
   // Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-  DoubleSolenoid leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 7);
+  DoubleSolenoid leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 5);
   DoubleSolenoid rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 6);
 
+  //clicky thingy
+  //DigitalInput limitSwitch = new DigitalInput(0);
+  //limitSwitch.get()
 
 
   CANSparkMax leftShooter = new CANSparkMax(RobotMap.leftShooterID, MotorType.kBrushless);
@@ -79,25 +90,39 @@ public class Robot extends TimedRobot {
     //make the motors follow each other
     backRight.follow(frontRight);
     backLeft.follow(frontLeft);
+
+    armLifter.setIdleMode(IdleMode.kBrake);
+    armLifter.getEncoder().setPosition(0);
+    
+    UsbCamera camera = CameraServer.startAutomaticCapture();
+    camera.setResolution(4000, 4000);
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
   @Override
   public void autonomousInit() {
     m_timer.restart();
+    leftSolenoid.set(DoubleSolenoid.Value.kOff);
+    rightSolenoid.set(DoubleSolenoid.Value.kOff);
+    // Alliance alliance = DriverStation.getAlliance();
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
+    autoAmpshotAndSend(true, 0.15);
     //autoAmpshot(true);
-    forwardAuto();
+    //shootInAuto();
+    //forwardAuto();
     // else if (m_timer.get() < 2.5) {
     //   m_robotDrive.stopMotor();
     //   armLifter.set(-0.25);
     // }
   }
 
+  public void disabledPeriodic() {
+    SmartDashboard.putNumber("Arm Encoder", armLifter.getEncoder().getPosition());
+  }
   /** This function is called once each time the robot enters teleoperated mode. */
   @Override
   public void teleopInit() {
@@ -112,13 +137,22 @@ public class Robot extends TimedRobot {
     m_robotDrive.arcadeDrive(-m_joystick.getY()*speed, -m_joystick.getX()*speed);
 
     // make arm move
+    double armEncoderCount = armLifter.getEncoder().getPosition();
     if (opperatorJoystick.getRawButton(RobotMap.y)) {
-      System.out.println("y");
-      armLifter.set(0.5);
+      if (armEncoderCount < RobotMap.ARM_UP_ENCODER_COUNT) {
+        armLifter.set(RobotMap.liftArmSpeed);
+      }
+      else {
+        armLifter.set(0);
+      }
     }
     else if (opperatorJoystick.getRawButton(RobotMap.a)) {
-      System.out.println("a");
-      armLifter.set(-0.5);
+      if (armEncoderCount > RobotMap.ARM_DOWN_ENCODER_COUNT) {
+        armLifter.set(-RobotMap.liftArmSpeed);
+      }
+      else {
+        armLifter.set(0);
+      }
     }
     else {
       armLifter.stopMotor();
@@ -128,20 +162,16 @@ public class Robot extends TimedRobot {
     // change speed if a button is pressed
     if(m_joystick.getRawButtonPressed(RobotMap.speedShiftButton)){
       if (speed == RobotMap.fastSpeed){
-        System.out.println("Slow Speed");
         speed = RobotMap.slowSpeed;
       }
       else if (speed == RobotMap.slowSpeed){
-        System.out.println("Fast Speed");
         speed = RobotMap.fastSpeed;
       }
     }
 
     // shoot
     if(m_joystick.getRawButtonPressed(RobotMap.trigger)){
-      System.out.println("trigger pressed");
       if (isShooting == true){
-        System.out.println("stop shooting");
         leftShooter.stopMotor();
         rightShooter.stopMotor();
         isShooting = false;
@@ -150,8 +180,6 @@ public class Robot extends TimedRobot {
         leftShooter.set(RobotMap.shootSpeed);
         rightShooter.set(-RobotMap.shootSpeed);
         isShooting = true;
-        System.out.println("start shooting");
-
       }
     }
     else if(opperatorJoystick.getRawButton(RobotMap.back) && isShooting == false){
@@ -166,24 +194,20 @@ public class Robot extends TimedRobot {
 
     // lift that lifter
     if(m_joystick.getRawButtonPressed(RobotMap.liftUpID)){
-      System.out.println("up");
       leftSolenoid.set(DoubleSolenoid.Value.kForward);
       rightSolenoid.set(DoubleSolenoid.Value.kForward);
     }
     else if(m_joystick.getRawButtonPressed(RobotMap.liftDownID)){
-      System.out.println("down");
       leftSolenoid.set(DoubleSolenoid.Value.kReverse);
       rightSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
     
     // succ and shoot
     if (opperatorJoystick.getRawButton(RobotMap.rt)){
-      System.out.println("rt");
       bottomIntake.set(RobotMap.armShootSpeed);
       topIntake.set(-RobotMap.armShootSpeed);
     }
     else if (opperatorJoystick.getRawButton(RobotMap.lt)){
-      System.out.println("lt");
       bottomIntake.set(-RobotMap.intakeSpeed);
       topIntake.set(RobotMap.intakeSpeed);
     }
@@ -191,6 +215,20 @@ public class Robot extends TimedRobot {
       bottomIntake.stopMotor();
       topIntake.stopMotor();
     }
+
+    // nudge input
+    // if (opperatorJoystick.getRawButton(RobotMap.rb)){
+    //   bottomIntake.set(RobotMap.nudgeSpeed);
+    //   topIntake.set(-RobotMap.nudgeSpeed);
+    // }
+    // else if (opperatorJoystick.getRawButton(RobotMap.lb)){
+    //   bottomIntake.set(-RobotMap.nudgeSpeed);
+    //   topIntake.set(RobotMap.nudgeSpeed);
+    // }
+    // else {
+    //   bottomIntake.stopMotor();
+    //   topIntake.stopMotor();
+    // }
   }
 
   /** This function is called once each time the robot enters test mode. */
@@ -199,27 +237,34 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    testAuto();
+  }
 
-  public void autoAmpshot(boolean red) {
-    leftShooter.set(-0.70);
-    rightShooter.set(0.70);
+  public void autoAmpshotAndSend(boolean turnRight, double driveForwardTime) {
     float m = 1;
-    if (red){
+    if (turnRight != true){
       m = -1;
     }
+
     // turn
-    if (m_timer.get() < 0.2) {
-      // turn
-      m_robotDrive.arcadeDrive(0, -0.5*m, false);
+    if (m_timer.get() < driveForwardTime) {
+      m_robotDrive.arcadeDrive(0.2, 0, false);
+      leftShooter.set(RobotMap.shootSpeed);
+      rightShooter.set(-RobotMap.shootSpeed);
     } 
-    else if (m_timer.get() < 1.4) {
+    else if (m_timer.get() < 2.55 + driveForwardTime){
+      shootInAuto(driveForwardTime);
+    }
+    else if (m_timer.get() < 2.8+driveForwardTime) {
+      m_robotDrive.arcadeDrive(0, -0.5*m, false);
+    }
+    else if (m_timer.get() < 3.8+driveForwardTime) {
       // send it
       m_robotDrive.arcadeDrive(0.5, 0.0, false);
-    } 
-    else {
-      leftShooter.stopMotor();
-      rightShooter.stopMotor();
+    }
+    else{
+      m_robotDrive.arcadeDrive(0, 0.0, false);
     }
   }
 
@@ -228,5 +273,28 @@ public class Robot extends TimedRobot {
       // send it
       m_robotDrive.arcadeDrive(0.5, 0.0, false);
     } 
+  }
+
+  public void shootInAuto(double driveForwardTime){
+    if (m_timer.get() < 2 + driveForwardTime){
+      leftShooter.set(RobotMap.shootSpeed);
+      rightShooter.set(-RobotMap.shootSpeed);
+    }
+    else if (m_timer.get() < 2.5 + driveForwardTime){
+      bottomIntake.set(RobotMap.armShootSpeed);
+      topIntake.set(-RobotMap.armShootSpeed);
+    }
+    else {
+      leftShooter.stopMotor();
+      rightShooter.stopMotor();
+      bottomIntake.stopMotor();
+      topIntake.stopMotor();
+    }
+  }
+
+  public void testAuto(){
+    if (m_timer.get() < 0.35) {
+      m_robotDrive.arcadeDrive(0, -0.5, false);
+    }
   }
 }
